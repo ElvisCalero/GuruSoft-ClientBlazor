3faa473f93e19eabaa9b0159691b46e14d5b9388
Elvis Calero <elviscalero962010@gmail.com>
2022-09-01 22:09:29 -0500

    upload files

---
+++ webPostgres.Domain/Entities/Peticion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace webPostgres.Domain.Entities
+{
+    public class Peticion
+    {
+        public int Id { get; set; }
+        public string Request { get; set; }
+        public DateTime Fecha_Request { get; set; }
+        public string Response { get; set; }
+        public DateTime Fecha_Response { get; set; }
+        public string Username { get; set; }
+    }
+}
